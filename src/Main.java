5d55e3b052a7f7febc5e9d8f8b93fb885fa21ee3
pc <muhammedmermer2004@gmail.com>
2023-08-07 21:57:35 +0300

    s2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("merhaba 1 ");
+        System.out.println("merhaba 2 ");
     }
 }
